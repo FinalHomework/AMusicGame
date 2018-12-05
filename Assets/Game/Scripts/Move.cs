7e5092073fd9d01fe2ed03b970a34a77b57ae003
architart <1437830419@qq.com>
2018-12-06 00:21:30 +0800

    ChangeOfKun
    Change

--- Assets/Game/Scripts/Move.cs
+++ Assets/Game/Scripts/Move.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class Move : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        //if (Input.GetKey(KeyCode.W))
-        {
+	void Update ()
+    {
             this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * 10f);
-        }
 	}
 }
